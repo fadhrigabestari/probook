1981b695966639edb6d759da3c2df3f268422f5f
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2018-11-24 21:20:42 +0700

    get JSON value from Google Books

--- webservice_book/src/services/BookServiceImpl.java
+++ webservice_book/src/services/BookServiceImpl.java
@@ -8,17 +8,23 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import javax.jws.WebService;
+// import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.Arrays;
 
-import probook.Book;
 
-@WebService(endpointInterface = "probook.BookService")  
+import models.Book;
+
+@WebService(endpointInterface = "services.BookService")  
 public class BookServiceImpl implements BookService {
 
     
     @Override
-    public Book[] searchBook(String title) throws IOException {
+    public Book[] searchBook(String title_input) throws IOException {
         Book[] array_book = new Book[1];
-        String get_url = "https://www.googleapis.com/books/v1/volumes?q=" + title;
+        title_input = title_input.replace(' ', '+');
+        String get_url = "https://www.googleapis.com/books/v1/volumes?q=" + title_input;
         System.out.println(get_url);
         URL url = new URL(get_url);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();   
@@ -30,16 +36,85 @@ public class BookServiceImpl implements BookService {
             // success
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-            // print result
-            System.out.println(response.toString());
+    
+            JSONObject json = new JSONObject(response.toString());
+            JSONArray books = json.getJSONArray("items");
+            int count_books = books.length();
+            array_book = new Book[count_books+1];
+
+            for (int i=0; i<count_books; i++) {
+
+                //get title
+                String title = books.getJSONObject(i).getJSONObject("volumeInfo").getString("title");
+                String idBook = books.getJSONObject(i).getString("id");
+
+                //get description
+                String description="-";
+                if (!books.getJSONObject(i).getJSONObject("volumeInfo").isNull("description")) 
+                    description = books.getJSONObject(i).getJSONObject("volumeInfo").getString("description");
+                
+                //get authors
+                String[] authors = new String[1];
+                if (!books.getJSONObject(i).getJSONObject("volumeInfo").isNull("authors")) {
+                    JSONArray authors_array = books.getJSONObject(i).getJSONObject("volumeInfo").getJSONArray("authors");
+                    authors = new String[authors_array.length()];
+                    for (int j=0; j< authors_array.length(); j++) {
+                        authors[j] = authors_array.getString(j);
+                    }
+                } else authors[0] = "-";
+                
+                
+                //get cover
+                String cover="-";
+                if (!books.getJSONObject(i).getJSONObject("volumeInfo").isNull("imageLinks")) 
+                    cover = books.getJSONObject(i).getJSONObject("volumeInfo").getJSONObject("imageLinks").getString("smallThumbnail");
+
+                //get categories
+                String[] categories = new String[1];
+                if (!books.getJSONObject(i).getJSONObject("volumeInfo").isNull("categories")) {
+                    JSONArray categories_array = books.getJSONObject(i).getJSONObject("volumeInfo").getJSONArray("categories");
+                    categories = new String[categories_array.length()];
+                    for (int j=0; j< categories_array.length(); j++) {
+                        categories[j] = categories_array.getString(j);
+                    }
+                } else categories[0] = "-";
+                
+                //get saleability and price
+                boolean saleability;
+                Double price;
+                String for_sale = books.getJSONObject(i).getJSONObject("saleInfo").getString("saleability");
+                if (for_sale.equals("NOT_FOR_SALE")) {
+                    saleability = false;
+                    price= 0.0;
+                } else {
+                    saleability = true;
+                    price = books.getJSONObject(i).getJSONObject("saleInfo").getJSONObject("listPrice").getDouble("amount");
+                }
+
+
+                //create new book
+                Book a_book = new Book();
+                a_book.setIdBook(idBook);
+                a_book.setTitle(title);
+                a_book.setAuthors(authors);
+                a_book.setCover(cover);
+                a_book.setDescription(description);
+                a_book.setCategories(categories);
+                a_book.setPrice(price);
+                a_book.setSaleability(saleability);
+
+                array_book[i] = a_book;
+            }
+            
         } else {
             System.out.println("GET request not worked");
         }
+
         return array_book;
     }
     
