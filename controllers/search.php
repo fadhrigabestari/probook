28fc42da06acdbc6417992c40c9e469647fa1038
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2018-11-26 16:17:29 +0700

    acces soap from php

--- controllers/search.php
+++ controllers/search.php
@@ -1,6 +1,10 @@
 <?php
 (basename(__FILE__) !== basename($_SERVER['SCRIPT_NAME'])) or die;
 
+ini_set('soap.wsdl_cache_enabled', 0);
+ini_set('soap.wsdl_cache_ttl', 900);
+ini_set('default_socket_timeout', 15);
+
 if (!isset($_REQUEST['search'])) {
     require 'views/search-book.php';
 } else {
@@ -16,5 +20,32 @@ if (!isset($_REQUEST['search'])) {
         where title like ? group by Books.idBook');
     $stmt->execute(["%$query%"]);
     $results = $stmt->fetchAll();
+
+    $wsdl = 'http://localhost:8081/api/books?wsdl';
+
+    $options = array(
+            'uri'=>'http://schemas.xmlsoap.org/soap/envelope/',
+            'style'=>SOAP_RPC,
+            'use'=>SOAP_ENCODED,
+            'soap_version'=>SOAP_1_1,
+            'cache_wsdl'=>WSDL_CACHE_NONE,
+            'connection_timeout'=>15,
+            'trace'=>true,
+            'encoding'=>'UTF-8',
+            'exceptions'=>true,
+        );
+    try {
+        $soap = new SoapClient($wsdl, $options);
+        $datas = $soap->searchBook($_REQUEST['search']);
+    }
+    catch(Exception $e) {
+        die($e->getMessage());
+    } 
+    // e($data->item[0->title);
+    foreach($datas->item as $data) {
+        e($data->title);
+    }
+    die;
+
     require 'views/search-result.php';
 }
